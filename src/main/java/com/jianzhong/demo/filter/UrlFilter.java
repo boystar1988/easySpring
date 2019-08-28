47f3a27a7977e4ca08ddf4d328cdc3ea7c977718
boystar <814411929@qq.com>
2019-08-29 00:54:01 +0800

    filter

---
+++ src/main/java/com/jianzhong/demo/filter/UrlFilter.java
@@ -0,0 +1,37 @@
+package com.jianzhong.demo.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Configuration
+@Slf4j
+@WebFilter(filterName = "urlFilter", urlPatterns = "/user/*")
+public class UrlFilter implements Filter
+{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("----------------------->过滤器被创建");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        String requestURI = req.getRequestURI();
+        if (!requestURI.contains("index") && !requestURI.contains("detail")) {
+            servletRequest.getRequestDispatcher("/401").forward(servletRequest, servletResponse);
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("----------------------->过滤器被销毁");
+    }
+}
